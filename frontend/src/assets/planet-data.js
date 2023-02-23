f7f01b7b8f93cc2fb4677fe06f777960ebb3bf11
Qmullens <you@example.com>
2023-02-23 13:35:17 -0600

    Added JS file for the PieChart data and added comp

---
+++ frontend/src/assets/planet-data.js
@@ -0,0 +1,33 @@
+export const planetChartData = {
+    type: "pie",
+    data: {
+      labels: ["77532", "77338", "77885", "76532"],
+      datasets: [
+        {
+          label: "Customers by Zipcode",
+          data: [0, 0, 1, 2,],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#4BC0C0",
+            "#9966FF",
+            "#FF9933",
+            "#99CC00",
+            "#FF69B4",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: "Number of Customers in the Zip code",
+      },
+    },
+  };
+  
+  export default planetChartData;
+  
---
+++ frontend/src/components/PlanetChart.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- canvas element for chart -->
+    <canvas id="planet-chart"></canvas>
+  </div>
+</template>
+
+
+<script>
+import { Chart, registerables } from 'chart.js'
+import planetChartData from '../assets/planet-data.js'
+
+//we have to register the registerables with Chart object
+Chart.register(...registerables);
+export default {
+  name: 'PlanetChart',
+  data() {
+    return {
+      planetChartData: planetChartData
+    }
+  },
+  //establish Chart object after mounting the component
+  mounted() {
+    const ctx = document.getElementById('planet-chart');
+    new Chart(ctx, this.planetChartData);
+  }
+}
+</script>
