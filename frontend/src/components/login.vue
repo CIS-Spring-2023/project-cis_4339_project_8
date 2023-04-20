556c31bf34ec7abb6b876b1609575b6b652df051
Qmullens <you@example.com>
2023-04-20 12:52:41 -0500

    Reloading old login due to major errors

--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -1,21 +1,22 @@
+// Reference: ChatGPT for some basis of code 
 <template>
-  <!-- Create a login form -->
+    <!-- Create a login form -->
   <div class="login-container">
     <h1>Dataplatform Login</h1>
     <form @submit.prevent="submitForm">
       <div class="form-group">
         <label for="email">Email:</label>
-        <!-- Bind email input value to email state property -->
+        <!-- Bind email input value to email data property -->
         <input type="email" id="email" v-model="email" required placeholder="example@example.com">
       </div>
       <div class="form-group">
         <label for="password">Password:</label>
-        <!-- Bind password input value to password state property -->
+        <!-- Bind password input value to password data property -->
         <input type="password" id="password" v-model="password" required placeholder="Password">
       </div>
       <div class="form-group">
         <label for="user-type">Select User Type:</label>
-        <!-- Bind user type select value to userType state property -->
+        <!-- Bind user type select value to userType data property -->
         <select id="user-type" v-model="userType">
           <option value="viewer">Viewer</option>
           <option value="editor">Editor</option>
@@ -33,132 +34,109 @@
     </div>
   </div>
 </template>
+
 <script>
-import { defineComponent } from 'vue'
-import { useStore } from 'pinia'
-
-export default defineComponent({
-  setup() {
-    const store = useStore()
-
-    const email = store.email
-    const password = store.password
-    const userType = store.userType
-    const loginError = store.loginError
-
-    function submitForm() {
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      userType: 'viewer',
+      loginError: ''
+    }
+  },
+  methods: {
+    submitForm() {
       // authenticate the user
       let authenticated = false;
-      if (userType.value === 'viewer') {
-        authenticated = email.value === 'viewer@example.com' && password.value === 'password123';
-        store.loginType = 'Viewer';
-      } else if (userType.value === 'editor') {
-        authenticated = email.value === 'editor@example.com' && password.value === 'password123';
-        store.loginType = 'Editor';
+      if (this.userType === 'viewer') {
+        authenticated = this.email === 'viewer@example.com' && this.password === 'password123';
+        this.$root.loginType = 'Viewer';
+      } else if (this.userType === 'editor') {
+        authenticated = this.email === 'editor@example.com' && this.password === 'password123';
+        this.$root.loginType = 'Editor';
       }
       if (authenticated) {
         // if the email and password match, redirect to the dashboard page
-        store.isAuthenticated = true;
-        // assuming router is also managed by Pinia store, you can push to dashboard using:
-        // store.$router.push({ name: 'dashboard' })
+        this.$router.push({ name: 'dashboard' });
+        this.$root.isAuthenticated = true;
       } else {
         // if the email and password don't match, set the loginError message
-        store.loginError = 'Invalid credentials. Please try again.';
+        this.loginError = 'Invalid credentials. Please try again.';
       }
     }
-
-    return {
-      email,
-      password,
-      userType,
-      loginError,
-      submitForm
-    }
   }
-})
+}
 </script>
+
 <style>
-  .login-container {
-    max-width: 400px;
-    margin: 0 auto;
-    text-align: center;
-    background-color: #f7f7f7;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
-  }
+.login-container {
+  max-width: 400px;
+  margin: 0 auto;
+  text-align: center;
+  background-color: #f7f7f7;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+}
 
-  h1 {
-    font-size: 36px;
-    margin-bottom: 20px;
-    color: #333;
-    font-family: 'Montserrat', sans-serif;
-  }
+h1 {
+  font-size: 32px;
+  margin-bottom: 20px;
+  color: #444;
+}
 
-  .form-group {
-    margin: 20px 0;
-  }
+.form-group {
+  margin: 20px 0;
+}
 
-  label {
-    display: block;
-    font-size: 18px;
-    font-weight: bold;
-    text-align: left;
-    margin-bottom: 10px;
-    color: #444;
-    font-family: 'Roboto', sans-serif;
-  }
+label {
+  display: block;
+  font-size: 18px;
+  font-weight: bold;
+  text-align: left;
+  margin-bottom: 10px;
+  color: #444;
+}
 
-  input[type="email"],
-  input[type="password"],
-  select {
-    width: 100%;
-    padding: 10px;
-    border: none;
-    border-radius: 4px;
-    box-sizing: border-box;
-    font-size: 16px;
-    margin-bottom: 10px;
-    background-color: #f5f5f5;
-    color: #333;
-    font-family: 'Roboto', sans-serif;
-  }
+input[type="email"],
+input[type="password"],
+select {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  font-size: 16px;
+  margin-bottom: 10px;
+}
 
-  button[type="submit"] {
-    background-color: #2a80b9;
-    color: white;
-    padding: 12px 20px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    font-size: 16px;
-    margin-top: 20px;
-    width: 100%;
-    font-family: 'Montserrat', sans-serif;
-    transition: background-color 0.2s ease-in-out;
-  }
+button[type="submit"] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+  margin-top: 20px;
+  width: 100%;
+}
 
-  button[type="submit"]:hover {
-    background-color: #1c5a7d;
-  }
-
-  .error {
-    color: red;
-    font-size: 14px;
-    margin-top: 10px;
-    font-family: 'Roboto', sans-serif;
-  }
-
-  .credentials {
-    margin-top: 20px;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    background-color: #f7f7f7;
-    text-align: center;
-    font-size: 18px;
-    font-weight: bold;
-    color: #444;
-    font-family: 'Montserrat', sans-serif;
-  }
+.error {
+  color: red;
+  font-size: 14px;
+  margin-top: 10px;
+}
+.credentials {
+  margin-top: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #f7f7f7;
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
+  color: #444;
+}
 </style>
