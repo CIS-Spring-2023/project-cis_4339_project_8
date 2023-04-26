2a74c8a729c9fb40de1b0d43d135a44cf7fc3e5f
ShaheerBhangar <89551817+ShaheerBhangar@users.noreply.github.com>
2023-04-26 13:15:58 -0500

    Update users.js
    Updated backend for users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,20 +1,29 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt');
+
+
+const org = process.env.ORG
+
 // Login endpoint
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
-  
+
     // Find user in the database
     const user = await db.findOne({ email });
-  
+
     if (!user) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
-  
+
     // Compare passwords
     const isPasswordValid = await bcrypt.compare(password, user.password);
-  
+
     if (!isPasswordValid) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }})
   app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
+    console.log('Server listening on port ${PORT}')
   })
+
+  emodule.exports = router;
